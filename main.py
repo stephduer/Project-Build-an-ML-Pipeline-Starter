937868c05dd6270a777bdcceb011b524dec1721c
Stephanie Duer <duer.steph@gmail.com>
2026-03-12 23:39:49 -0400

    init commit

--- main.py
+++ main.py
@@ -50,22 +50,45 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+           _ = mlflow.run(
+               f"src/basic_cleaning",
+               "main",
+               env_manager="conda",
+               parameters={
+                   "input_artifact": "sample.csv:latest",
+                   "output_artifact": "clean_sample.csv",
+                   "output_type": "clean_sample",
+                   "output_description": "Cleaned version of sample.csv",
+                   "min_price": float(config["etl"]["min_price"]),
+                   "max_price": float(config["etl"]["max_price"])
+               }
+           )
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+           _ = mlflow.run(
+               f"src/data_check",
+               "main",
+               env_manager="conda",
+               parameters={
+                   "csv": "clean_sample.csv:latest",
+                   "ref": "clean_sample.csv:reference",
+                   "kl_threshold": float(config["data_check"]["kl_threshold"]),
+                   "min_price": float(config["etl"]["min_price"]),
+                   "max_price": float(config["etl"]["max_price"])
+               }
+           )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                'main',
+                parameters = {
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                }
+            )
 
         if "train_random_forest" in active_steps:
 
@@ -77,19 +100,30 @@ def go(config: DictConfig):
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                f"src/train_random_forest",
+                'main',
+                parameters = {
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": "/home/sduer/D501/Project-Build-an-ML-Pipeline-Starter/rf_config.json",
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export",                    
+                }
+            )
 
         if "test_regression_model" in active_steps:
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                'main',
+                parameters = {
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                }
+            )
 
 
 if __name__ == "__main__":
